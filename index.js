8ad7905eeb00f4cd1699c3a9c821c764433eaab4
khalid jamal <kh.jamal95@gmail.com>
2018-08-06 22:20:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function increaseRankBy(n) {
 
 function deepestChild(){
   var nodes = document.querySelectorAll('#grand-node div');
-  var mostDeep = nodes[nodes];
+  var mostDeep = nodes[nodes.length- 1];
 
 return mostDeep
   
