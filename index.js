130e70d4f29b9610143c93e06ba4345865188e63
khalid jamal <kh.jamal95@gmail.com>
2018-08-06 22:43:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ return document.querySelector('#nested .target')}
 
 
 function increaseRankBy(n) {
-  const rankedLists = document.querySelectorAll('.ranked-list')
+    let ranked = document.querySelectorAll('.ranked-list')
+    for (let i = 0, l = ranked.length; i < l; i++) {
 
-  for (let i = 0, l = rankedLists.length; i < l; i++) {
-    let children = rankedLists[i].children
+        let child = ranked[i].children
 
-    for (let j = 0, k = children.length; j < k; j++) {
-      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+        for (let o = 0, p = child.length; o < p; o++) {
+            child[0].innerHTML = parseInt(child[o].innerHTML + n)
+        }
     }
-  }
 }
 
 
