402a7e85e197ae91d733d9edc3794be675328806
khalid jamal <kh.jamal95@gmail.com>
2018-08-06 22:25:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,16 +24,3 @@ function deepestChild() {
     return deepest;
 
 }
-
-// function deepestChild() {
-//   let node = document.getElementById('grand-node')
-//   let nextNode = node.children[0]
-
-//   while (nextNode) {
-//     node = nextNode
-//     nextNode = node.children[0]
-//   }
-
-//   return node
-// }
-
